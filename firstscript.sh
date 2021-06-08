c604cba66970fe9166e61dc51d376eb3c314d60f
ymanucharian <manucharian1991@gmail.com>
2021-06-08 16:19:16 +0300

    Delete firstscript.sh

--- firstscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-access_token=$(curl -X POST \
--H 'content-type:application/x-www-form-urlencoded' \
--d 'username=ymanucharian&password=secret' \
-http://localhost:8000/auth) | jq -r ".access_token"
-echo "$access_token"
-curl -X GET \
--H "Authorization: Bearer ${access_token}" http://localhost:8000/image --output /Users/user/Documents/AutomatedTesting/Lessons/image7.png
